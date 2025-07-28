bc78505939c787c340fffddb527b7c47a5892beb
Ashutoshbirje <ashutoshbirje4@gmail.com>
2025-07-28 18:42:00 +0530

    ok

---
+++ 07_Forms/app1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from "./component/Form.js";
+
+function App() {
+  return (
+    <div className="App">
+       <h2>Hello</h2>
+       <Form></Form>
+    </div>
+  );
+}
+
+export default App;
