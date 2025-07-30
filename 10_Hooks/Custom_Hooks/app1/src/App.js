faeaf453c5f8e6b2966f281c31f6f68a8166ee86
Ashutoshbirje <ashutoshbirje4@gmail.com>
2025-07-30 23:50:29 +0530

    ok

---
+++ 10_Hooks/Custom_Hooks/app1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/Home.js';
+
+function App() {
+  return (
+    <div className="App">
+       <Home/>
+       {/* <h1>Welcome to Platform</h1> */}
+    </div>
+  );
+}
+
+export default App;
